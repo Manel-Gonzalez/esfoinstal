d012d147e224b74773baa1d17d118d5af046196a
Manel-Gonzalez <72015770+Manel-Gonzalez@users.noreply.github.com>
2025-03-26 17:12:23 +0100

    test with supabase

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const {Sequelize} = require("sequelize");
-const dbConfig = require("../config/config")["development"];
-const sequelize = new Sequelize(dbConfig);
+const supabase = require("../supabaseClient");
 
+/* const {Sequelize} = require("sequelize");
+ */ /* const sequelize = new Sequelize(dbConfig);
+ */
 const testRoute = require("../routes/testRoute");
 const PORT = process.env.PORT || 5000;
 
@@ -13,16 +14,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-sequelize
+/* sequelize
   .authenticate()
   .then(() => console.log("✅ Connecting to SQLite DB"))
-  .catch((err) => console.error("❌ Error trying to conect to the DB", err));
+  .catch((err) => console.error("❌ Error trying to conect to the DB", err)); */
 
-app.get("/", (req, res) => {
-  res.send("🚀 Backend working with SQLite & Express");
+app.get("/api/test", async (req, res) => {
+  const {data, error} = await supabase.from("test").select("message").limit(1);
+
+  if (error) {
+    console.error("❌ Supabase error:", error);
+    return res.status(500).json({error: "Error fetching data"});
+  }
+
+  res.json({message: data?.[0]?.message || "Sin datos"});
 });
-
-app.use("/api/test", testRoute);
 
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
@@ -30,5 +36,9 @@ if (require.main === module) {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
+
+app.get("/", (req, res) => {
+  res.send("🚀 Backend working with Supabase!");
+});
 
 module.exports = app;
