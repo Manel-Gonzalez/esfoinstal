743bcf7b5fd4bd42155b4344bb154e223192664a
Manel-Gonzalez <72015770+Manel-Gonzalez@users.noreply.github.com>
2025-03-26 15:39:41 +0100

    setting for deployment

--- backend/routes/testRoute.js
+++ backend/routes/testRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({message: "🚀 LAPAMPARA"});
+  res.json({message: "LAPAMPARA"});
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const {Sequelize} = require("sequelize");
-const dbConfig = require("./config/config.json")["development"];
+const dbConfig = require("./config/config.js")["development"];
 const sequelize = new Sequelize(dbConfig);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 sequelize
   .authenticate()
   .then(() => console.log("✅ Connecting to SQLite DB"))
   .catch((err) => console.error("❌ Error trying to conect to the DB", err));
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("🚀 Backend working with SQLite & Express");
@@ -24,8 +24,6 @@ app.get("/", (req, res) => {
 const testRoute = require("./routes/testRoute");
 app.use("/api/test", testRoute);
 
-/* const homeRoutes = require("./routes/home");
-app.use("/api/home", homeRoutes); */
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
